a5b8aadb41c631e2251628991c9d2c69190a3972
Leigh de Paor <leigh@spreachgeal.com>
2021-10-21 15:45:04 +0100

    Add files

---
+++ Bastion-SW-Install.ps1
@@ -0,0 +1,105 @@
+<#
+    .SYNOPSIS
+    Install required software from internet on bastion vm's
+
+    .DESCRIPTION
+    Gets latest version number of PowerShell from internet, downloads & installs
+    Uses list of required software based on machine name - datavm/webvm/qavm
+    Downloads & installs required software
+    
+    .OUTPUTS
+    Screen as collection of tabulated text
+
+    .EXAMPLE
+    PS> .\Bastion-SW-Install.ps1
+    
+#>
+
+#Install latest version of PowerShell
+$url = 'https://github.com/PowerShell/PowerShell/releases/latest'
+#$request = [System.Net.WebRequest]::Create($url)
+#$response = ([System.Net.WebRequest]::Create($url)).GetResponse()
+$realTagUrl = (([System.Net.WebRequest]::Create($url)).GetResponse()).ResponseUri.OriginalString
+
+$version = $realTagUrl.split('/')[-1].Trim('v')
+Write-Host "Latest PowerShell version on github: $($version)" -Foregroundcolor Yellow
+
+$fileName = "PowerShell-$($version)-win-x64.msi"
+$Outfile = "$($env:TEMP)\$($fileName)"
+$realDownloadUrl = "$($realTagUrl.Replace('tag', 'download'))/$($fileName)"
+
+Write-Host "Downloading $($fileName) from github..." -Foregroundcolor Yellow
+Invoke-WebRequest -Uri $realDownloadUrl -OutFile $Outfile
+if(Test-Path $env:TEMP/$fileName){
+	Write-Host "Installing $($fileName)..." -Foregroundcolor Yellow
+	$arguments = "/i `"$($Outfile)`" /passive"
+	Start-Process msiexec.exe -ArgumentList $arguments -Wait
+}
+
+##################################
+
+function DownloadAndRunExe($url, $arguments){
+	Write-Host "Downloading $($url)" -Foregroundcolor Yellow
+	Invoke-WebRequest -Uri $url -OutFile "$($env:TEMP)\install.exe"
+	if($arguments -notlike ""){
+		Write-Host "Attempting to install $($url) with arguments" -Foregroundcolor Yellow
+		Start-Process -filepath "$($env:TEMP)\install.exe" -wait -ArgumentList $arguments
+	}else{
+		Write-Host "Attempting to install $($url) with no arguments" -Foregroundcolor Yellow
+		Start-Process -filepath "$($env:TEMP)\install.exe" -wait 
+	}
+}
+
+$urls = @()
+
+switch( hostname ){
+	{$_ -match "datavm"}{
+		#data team software
+		write-host "datavm detected, selecting software" -Foregroundcolor Green
+		$urls += @{
+			"url" = 'https://sqlopsbuilds.azureedge.net/stable/4a45ba7cf20dd4129f1a08e5e776dfb33e3d1d1e/azuredatastudio-windows-setup-1.32.0.exe'
+			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSERS","/MERGETASKS=!runcode")
+		}
+		$urls += @{
+			"url" = 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
+			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode","/SP-")
+		}
+		$urls += @{
+			"url" = 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7BCB508FFA-6B4E-A202-A703-3711137C171D%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dprefers%26ap%3Dx64-stable-statsdef_1%26installdataindex%3Dempty/update2/installers/ChromeSetup.exe'
+			#"url" = 'https://ninite.com/7zip-chrome-filezilla-putty-sumatrapdf-vscode-winscp/ninite.exe'
+			"arguments" = ""
+		}
+	}
+	{$_ -match "qavm"}{
+		write-host "qavm detected, selecting software" -Foregroundcolor Green
+		#QA team software
+		$urls += @{
+			"url" = 'https://sqlopsbuilds.azureedge.net/stable/4a45ba7cf20dd4129f1a08e5e776dfb33e3d1d1e/azuredatastudio-windows-setup-1.32.0.exe'
+			"arguments" = "/VERYSILENT /NORESTART /CURRENTUSERS"
+		}
+		$urls += @{
+			"url" = 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
+			"arguments" = "/VERYSILENT /NORESTART /CURRENTUSERS"
+		}
+		$urls += @{
+			"url" = 'https://ninite.com/7zip-chrome-eclipse-filezilla-notepadplusplus-putty-sumatrapdf-vscode-winscp/ninite.exe'
+			"arguments" = ""
+		}
+	}
+	{$_ -match "webvm"}{
+		write-host "webvm detected, selecting software" -Foregroundcolor Green
+		#Web/API team software
+		$urls += @{
+			"url" = 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
+			"arguments" = "/VERYSILENT /NORESTART /CURRENTUSERS"
+		}
+		$urls += @{
+			"url" = 'https://ninite.com/7zip-chrome-filezilla-notepadplusplus-putty-sumatrapdf-vscode-winscp/ninite.exe'
+			"arguments" = ""
+		}
+	}
+}
+
+foreach($item in $urls){
+	DownloadAndRunExe $item.url $item.arguments
+}
---
+++ Bastion-secure-post_deploy.ps1
@@ -0,0 +1,164 @@
+#post deploy commands
+
+######################################
+function set-reg_keys{
+Param(
+    #Parameter that can be modified to control which resource groups this runs against
+    [Parameter(
+        Mandatory=$false,
+        ValueFromPipeline=$true,
+        HelpMessage="Enter one or more Windows registry setting as a hashtable object."    
+        )
+    ]
+    [System.Collections.Hashtable]
+    $RegSet = $null
+)
+	if(!($RegSet.count -eq 5)){
+		Write-Host "RegSet parameter requires 5 key pairs in the hashtable: Path, Name, Type, Value, Hive"
+	}else{
+		switch($RegSet.Hive){
+			{$_ -eq "HKEY_LOCAL_MACHINE"}{$Hive = "HKLM"}
+			{$_ -eq "HKEY_CURRENT_USER"}{$Hive = "HKCU"}
+			default {$Hive = $false; break}
+		}
+		switch($RegSet.Type){
+			{$_ -eq "REG_SZ"}{$Type = "String"}
+			{$_ -eq "REG_EXPAND_SZ"}{$Type = "ExpandString"}
+			{$_ -eq "REG_BINARY"}{$Type = "Binary"}
+			{$_ -eq "REG_DWORD"}{$Type = "DWord"}
+			{$_ -eq "REG_MULTI_SZ"}{$Type = "MultiString"}
+			{$_ -eq "REG_QWORD"}{$Type = "Qword"}
+			default {$Type = $false; break}
+		}
+		if(($Hive -eq $false) -or ($Type -eq $false)){
+			Write-Host "Error with type or hive specified, cannot continue" -Foregroundcolor Red
+			break
+		}
+		$Path = $RegSet.Path
+		$Name = $RegSet.Name
+		$Value = $RegSet.Value
+		#set-location -path "$($Hive):\$($Path)"
+		try{
+			Get-Item -path "$($Hive):\$($Path)\$($Name)" -erroraction stop
+			if((Get-ItemProperty -Path "$($Hive):\$($Path)" -Name $Name).$Name -eq $Value){
+				Write-Host "$($Name) is already set to $($Value), no further action required."
+			}else{
+				Set-ItemProperty -Path "$($Hive):\$($Path)" -Name $Name -Value $Value -Type $Type
+				if((Get-ItemProperty -Path "$($Hive):\$($Path)" -Name $Name).$Name -eq $Value){
+					Write-Host "$($Name) succesfully set to $($Value), no further action required." -Foregroundcolor Green
+				}else{
+					Write-Host "Error setting $($Hive):\$($Path)\$($Name) to $($Value), please remediate." -Foregroundcolor Red
+				}
+			}
+		}catch{
+			Write-Host "Item $($Name) does not exist at $($Hive):\$($Path), attempting to create"
+			Set-ItemProperty -Path "$($Hive):\$($Path)" -Name $Name -Value $Value -Type $Type
+			if((Get-ItemProperty -Path "$($Hive):\$($Path)" -Name $Name).$Name -eq $Value){
+				Write-Host "$($Name) succesfully set to $($Value), no further action required." -Foregroundcolor Green
+			}else{
+				Write-Host "Error setting $($Hive):\$($Path)\$($Name) to $($Value), please remediate." -Foregroundcolor Red
+			}
+		}		
+	}
+}
+######################################
+
+$RegSettings = @()
+
+#turn off autoplay
+$RegSetting = @{
+	"Hive" = "HKEY_LOCAL_MACHINE"
+	"Path" = "Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+	"Name" = "NoDriveTypeAutoRun"
+	"Type" = "REG_DWORD"
+	"Value" = 255
+}
+$RegSettings += $RegSetting
+
+
+#turn off autoplay for nonvolume devices
+$RegSetting = @{
+	"Hive" = "HKEY_LOCAL_MACHINE"
+	"Path" = "Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+	"Name" = "NoAutoplayfornonVolume"
+	"Type" = "REG_DWORD"
+	"Value" = 1
+}
+$RegSettings += $RegSetting
+
+#don't cache logon credentials
+$RegSetting = @{
+	"Hive" = "HKEY_LOCAL_MACHINE"
+	"Path" = "Software\Microsoft\Windows Nt\CurrentVersion\Winlogon"
+	"Name" = "CachedLogonsCount"
+	"Type" = "REG_SZ"
+	"Value" = 0
+}
+$RegSettings += $RegSetting
+
+#Restrict Null Session Access
+$RegSetting = @{
+	"Hive" = "HKEY_LOCAL_MACHINE"
+	"Path" = "SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters"
+	"Name" = "RestrictNullSessAccess"
+	"Type" = "REG_DWORD"
+	"Value" = 1
+}
+$RegSettings += $RegSetting
+
+#Require SMB2 signing
+$RegSetting = @{
+	"Hive" = "HKEY_LOCAL_MACHINE"
+	"Path" = "SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters"
+	"Name" = "RequireSecuritySignature"
+	"Type" = "REG_DWORD"
+	"Value" = 1
+}
+$RegSettings += $RegSetting
+
+$RegSetting = @{
+	"Hive" = "HKEY_LOCAL_MACHINE"
+	"Path" = "SYSTEM\CurrentControlSet\Services\LanmanWorkStation\Parameters"
+	"Name" = "RequireSecuritySignature"
+	"Type" = "REG_DWORD"
+	"Value" = 1
+}
+$RegSettings += $RegSetting
+
+#turn off autoplay for default user
+$RegSetting = @{
+	"Hive" = "HKEY_USERS"
+	"Path" = "DEFAULT\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+	"Name" = "NoDriveTypeAutoRun"
+	"Type" = "REG_DWORD"
+	"Value" = 255
+}
+#$RegSettings += $RegSetting
+######################################
+Clear-Host
+Write-Host "Setting reg keys" -Foregroundcolor Yellow
+
+foreach($Item in $RegSettings){
+	set-reg_keys -RegSet $Item
+}
+######################################
+Write-Host "Ensuring SMB1 is off" -Foregroundcolor Yellow
+if((Get-WindowsOptionalFeature -Online -FeatureName smb1protocol).state -notlike "DisabledWithPayloadRemoved"){Disable-WindowsOptionalFeature -Online -FeatureName smb1protocol}
+Write-Host "Renaming Guest Account" -Foregroundcolor Yellow
+wmic useraccount where "name='Guest'" rename "GuessThis"
+Write-Host "Setting netowrk profile to public" -Foregroundcolor Yellow
+Set-NetConnectionProfile -InterfaceAlias Ethernet -NetworkCategory "Public"
+Write-Host "Adding Nuget" -Foregroundcolor Yellow
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force | Out-Null
+Write-Host "Setting PSGallery as trusted repository" -Foregroundcolor Yellow
+Set-PSRepository -name PSGallery -InstallationPolicy trusted
+Write-Host "Installing Windows Update PowerShell module" -Foregroundcolor Yellow
+Install-Module -Name PSWindowsUpdate
+Write-Host "Scheduling Windows updates to run on system boot" -Foregroundcolor Yellow
+$trigger = New-JobTrigger -AtStartup -RandomDelay 00:00:30
+$ScheduledJobOption = New-ScheduledJobOption -RunElevated
+Register-ScheduledJob -Trigger $trigger -Name WindowsUpdate -ScriptBlock {Get-WindowsUpdate -Install -confirm:$false -forceinstall -autoreboot -acceptall} -ScheduledJobOption $ScheduledJobOption -credential (Get-Credential -message "enter password for scheduled updates command" -username (whoami))
+Write-Host "Setting Windows Defender preferences" -Foregroundcolor Yellow
+Set-MpPreference -ScanParameters FullScan -ScanScheduleDay Everyday -DisableIntrusionPreventionSystem 0 -DisableRealtimeMonitoring 0 -DisableEmailScanning 0 -DisableRemovableDriveScanning 0 -EnableNetworkProtection Enabled -EnableControlledFolderAccess Enabled -verbose
+Write-Host "Running Windows updates, system may reboot" -Foregroundcolor Yellow
+Get-WindowsUpdate -Install -confirm:$false -forceinstall -autoreboot -acceptall
