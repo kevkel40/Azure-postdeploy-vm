674c3fbd1c9f8587779620857f207d87efa7068d
Leigh de Paor <leigh@spreachgeal.com>
2021-11-28 17:14:46 +0000

    Tidy up repetition

--- Bastion-SW-Install.ps1
+++ Bastion-SW-Install.ps1
@@ -23,7 +23,7 @@ function get-powershelllatest ($realTagUrl, $version){
 
 	Write-Host "Downloading $($fileName) from github..." -Foregroundcolor Yellow
 	Invoke-WebRequest -Uri $realDownloadUrl -OutFile $Outfile
-	if(Test-Path $env:TEMP/$fileName){
+	if(Test-Path $Outfile){
 		Write-Host "Installing $($fileName)..." -Foregroundcolor Yellow
 		$arguments = "/i `"$($Outfile)`" /passive"
 		Start-Process msiexec.exe -ArgumentList $arguments -Wait
@@ -59,7 +59,7 @@ function get-gitwinlatest ($realTagUrl, $version){
 	$realDownloadUrl = "$($realTagUrl.Replace('tag', 'download'))/$($fileName)"
 	Write-Host "Downloading $($fileName) from github..." -Foregroundcolor Yellow
 	Invoke-WebRequest -Uri $realDownloadUrl -OutFile $Outfile
-	if(Test-Path $env:TEMP/$fileName){
+	if(Test-Path $Outfile){
 		Write-Host "Installing $($fileName)..." -Foregroundcolor Yellow
 		$arguments = @("/VERYSILENT","/NORESTART","/CURRENTUSER")
 		Start-Process $Outfile -ArgumentList $arguments -Wait
@@ -92,7 +92,7 @@ $url = 'https://dl.pstmn.io/download/latest/win64'
 $fileName = "Postman-win64-Setup.exe"
 $Outfile = "$($env:TEMP)\$($fileName)"
 Invoke-WebRequest -Uri $url -Outfile $Outfile
-if(Test-Path $env:TEMP/$fileName){
+if(Test-Path $Outfile){
   Write-Host "Installing $($fileName)..." -Foregroundcolor Yellow
   $arguments = @("/s") #??
   Start-Process $Outfile -ArgumentList $arguments -Wait
@@ -123,56 +123,56 @@ function DownloadAndRunExeMSI($url, $arguments){
 	}
 }
 
+#azure-cli latest version
+$url = 'https://aka.ms/installazurecliwindows'
+$fileName = "AzureCLI.msi"
+$Outfile = "$($env:TEMP)\$($fileName)"
+Invoke-WebRequest -Uri $url -Outfile $Outfile
+if(Test-Path $Outfile){
+  Write-Host "Installing $($fileName)..." -Foregroundcolor Yellow
+  $arguments = @("/I $($Outfile)", "/quiet") #??
+  Start-Process msiexec.exe -ArgumentList $arguments -Wait
+}
+##################################
+
 $urls = @()
+
+#common software
+write-host "selecting common software" -Foregroundcolor Green
+
+$urls += @{
+	"url" = 'https://sqlopsbuilds.azureedge.net/stable/4a45ba7cf20dd4129f1a08e5e776dfb33e3d1d1e/azuredatastudio-windows-setup-1.32.0.exe'
+	"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode")
+}
+$urls += @{
+	"url" = 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
+	"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode","/SP-")
+}
+$urls += @{
+	"url" = 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B993F11EB-8372-361A-18C9-9B64A947F988%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dtrue%26ap%3Dx64-stable-statsdef_0%26brand%3DGCEA/dl/chrome/install/googlechromestandaloneenterprise64.msi'
+	"arguments" = @("MSI")
+}
+$urls += @{
+	"url" = 'https://cfhcable.dl.sourceforge.net/project/sevenzip/7-Zip/19.00/7z1900.exe'
+	"arguments" = @("/S")
+}
+$urls += @{
+	"url" = 'https://az764295.vo.msecnd.net/stable/6cba118ac49a1b88332f312a8f67186f7f3c1643/VSCodeUserSetup-x64-1.61.2.exe'
+	"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode")
+}
+$urls += @{
+	"url" = 'https://ninite.com/filezilla-notepadplusplus-putty-sumatrapdf-winscp/ninite.exe'
+	"arguments" = ""
+}
 
 switch( hostname ){
 	{$_ -match "datavm"}{
 		#data team software
 		write-host "datavm detected, selecting software" -Foregroundcolor Green
-		$urls += @{
-			"url" = 'https://sqlopsbuilds.azureedge.net/stable/4a45ba7cf20dd4129f1a08e5e776dfb33e3d1d1e/azuredatastudio-windows-setup-1.32.0.exe'
-			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode")
-		}
-		$urls += @{
-			"url" = 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
-			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode","/SP-")
-		}
-		$urls += @{
-			"url" = 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B993F11EB-8372-361A-18C9-9B64A947F988%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dtrue%26ap%3Dx64-stable-statsdef_0%26brand%3DGCEA/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-			"arguments" = @("MSI")
-		}
-		$urls += @{
-			"url" = 'https://cfhcable.dl.sourceforge.net/project/sevenzip/7-Zip/19.00/7z1900.exe'
-			"arguments" = @("/S")
-		}
-		$urls += @{
-			"url" = 'https://az764295.vo.msecnd.net/stable/6cba118ac49a1b88332f312a8f67186f7f3c1643/VSCodeUserSetup-x64-1.61.2.exe'
-			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode")
-		}
 	}
 	{$_ -match "qavm"}{
 		write-host "qavm detected, selecting software" -Foregroundcolor Green
 		#QA team software
-		$urls += @{
-			"url" = 'https://sqlopsbuilds.azureedge.net/stable/4a45ba7cf20dd4129f1a08e5e776dfb33e3d1d1e/azuredatastudio-windows-setup-1.32.0.exe'
-			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode")
-		}
-		$urls += @{
-			"url" = 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
-			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode","/SP-")
-		}
-		$urls += @{
-			"url" = 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B993F11EB-8372-361A-18C9-9B64A947F988%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dtrue%26ap%3Dx64-stable-statsdef_0%26brand%3DGCEA/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-			"arguments" = @("MSI")
-		}
-		$urls += @{
-			"url" = 'https://cfhcable.dl.sourceforge.net/project/sevenzip/7-Zip/19.00/7z1900.exe'
-			"arguments" = @("/S")
-		}
-		$urls += @{
-			"url" = 'https://az764295.vo.msecnd.net/stable/6cba118ac49a1b88332f312a8f67186f7f3c1643/VSCodeUserSetup-x64-1.61.2.exe'
-			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode")
-		}
 		$urls += @{
 			"url" = 'https://dbvis.com/product_download/dbvis-12.1.4/media/dbvis_windows-x64_12_1_4_jre.exe'
 			"arguments" = "-q"
@@ -239,57 +239,38 @@ switch( hostname ){
 		write-host "webvm detected, selecting software" -Foregroundcolor Green
 		#Web/API team software
 		$urls += @{
-			"url" = 'https://sqlopsbuilds.azureedge.net/stable/4a45ba7cf20dd4129f1a08e5e776dfb33e3d1d1e/azuredatastudio-windows-setup-1.32.0.exe'
-			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode")
-		}
-		$urls += @{
-			"url" = 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
-			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode","/SP-")
-		}
-		$urls += @{
-			"url" = 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B993F11EB-8372-361A-18C9-9B64A947F988%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dtrue%26ap%3Dx64-stable-statsdef_0%26brand%3DGCEA/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-			"arguments" = @("MSI")
-		}
-		$urls += @{
-			"url" = 'https://cfhcable.dl.sourceforge.net/project/sevenzip/7-Zip/19.00/7z1900.exe'
-			"arguments" = @("/S")
-		}
-		$urls += @{
-			"url" = 'https://az764295.vo.msecnd.net/stable/6cba118ac49a1b88332f312a8f67186f7f3c1643/VSCodeUserSetup-x64-1.61.2.exe'
-			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode")
-		}
-		$urls += @{
 			"url" = 'https://ninite.com/filezilla-notepadplusplus-putty-sumatrapdf-winscp/ninite.exe'
 			"arguments" = ""
 		}
+		#redis cli
+		$url = "https://github.com/microsoftarchive/redis/releases/download/win-3.2.100/Redis-x64-3.2.100.msi"
+		$fileName = $url.split("/")[-1]
+		$Outfile = "$($env:TEMP)\$($fileName)"
+		Write-Host "Downloading $($fileName)..." -Foregroundcolor Yellow
+		Invoke-WebRequest -Uri $url -Outfile $Outfile
+		if(Test-Path $Outfile){
+		  Write-Host "Installing $($fileName)..." -Foregroundcolor Yellow
+		 $arguments = @("/I $($Outfile)", "/quiet")
+		  Start-Process msiexec.exe -ArgumentList $arguments -Wait
+		}
+
+		#install stunnel
+		$url = "https://www.stunnel.org/downloads.html"
+		$fileName = ((Invoke-WebRequest -uri $url).links |Where-Object{$_.href -like "*win64-installer.exe"}|Where-Object{$_.href -notlike "*/beta/*"}).href.split("/")[-1]
+		$url = "https://www.stunnel.org/$(((Invoke-WebRequest -uri $url).links |Where-Object{$_.href -like '*win64-installer.exe'}|Where-Object{$_.href -notlike '*/beta/*'}).href)"
+		$Outfile = "$($env:TEMP)\$($fileName)"
+		Write-Host "Downloading $($fileName)..." -Foregroundcolor Yellow
+		Invoke-WebRequest -Uri $url -Outfile $Outfile
+		if(Test-Path $Outfile){
+		  Write-Host "Installing $($fileName)..." -Foregroundcolor Yellow
+		  $arguments = @("/AllUsers", "/S")
+		  Start-Process $Outfile -ArgumentList $arguments -Wait
+		}
+		#need to configure stunnel
 	}
 	{$_ -match "infravm"}{
 		write-host "Infravm detected, selecting software" -Foregroundcolor Green
-		#Web/API team software
-		$urls += @{
-			"url" = 'https://sqlopsbuilds.azureedge.net/stable/4a45ba7cf20dd4129f1a08e5e776dfb33e3d1d1e/azuredatastudio-windows-setup-1.32.0.exe'
-			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode")
-		}
-		$urls += @{
-			"url" = 'https://download.microsoft.com/download/A/E/3/AE32C485-B62B-4437-92F7-8B6B2C48CB40/StorageExplorer.exe'
-			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode","/SP-")
-		}
-		$urls += @{
-			"url" = 'https://dl.google.com/tag/s/appguid%3D%7B8A69D345-D564-463C-AFF1-A69D9E530F96%7D%26iid%3D%7B993F11EB-8372-361A-18C9-9B64A947F988%7D%26lang%3Den%26browser%3D4%26usagestats%3D0%26appname%3DGoogle%2520Chrome%26needsadmin%3Dtrue%26ap%3Dx64-stable-statsdef_0%26brand%3DGCEA/dl/chrome/install/googlechromestandaloneenterprise64.msi'
-			"arguments" = @("MSI")
-		}
-		$urls += @{
-			"url" = 'https://cfhcable.dl.sourceforge.net/project/sevenzip/7-Zip/19.00/7z1900.exe'
-			"arguments" = @("/S")
-		}
-		$urls += @{
-			"url" = 'https://az764295.vo.msecnd.net/stable/6cba118ac49a1b88332f312a8f67186f7f3c1643/VSCodeUserSetup-x64-1.61.2.exe'
-			"arguments" = @("/VERYSILENT","/NORESTART","/CURRENTUSER","/MERGETASKS=!runcode")
-		}
-		$urls += @{
-			"url" = 'https://ninite.com/filezilla-notepadplusplus-putty-sumatrapdf-winscp/ninite.exe'
-			"arguments" = ""
-		}
+		#Infra team software
 	}
 }
 
