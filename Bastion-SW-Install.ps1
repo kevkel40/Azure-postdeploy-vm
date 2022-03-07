3697e932c11da306396cc3f40e8c524ddeb25b87
Leigh de Paor <leigh@spreachgeal.com>
2022-03-07 15:12:09 +0000

    Update Bastion-SW-Install.ps1

--- Bastion-SW-Install.ps1
+++ Bastion-SW-Install.ps1
@@ -15,6 +15,9 @@
     
 #>
 
+#allow insrtalls to work
+try{Set-MpPreference -EnableControlledFolderAccess Disabled -ErrorAction stop}catch{exit 1}
+
 ##### - PowerShell Core - #####
 function get-powershelllatest ($realTagUrl, $version){
 	$fileName = "PowerShell-$($version)-win-x64.msi"
@@ -294,5 +297,7 @@ if((wsl --status).count -gt 50 ){
 }else{
   Write-Verbose "Windows subsystem for Linux already installed"
 }
+
+try{Set-MpPreference -ScanParameters FullScan -ScanScheduleDay Everyday -DisableIntrusionPreventionSystem 0 -DisableRealtimeMonitoring 0 -DisableEmailScanning 0 -DisableRemovableDriveScanning 0 -EnableNetworkProtection Enabled -EnableControlledFolderAccess Enabled -ScanScheduleTime 12:00 -RemediationScheduleTime 13:00 -SignatureScheduleTime 11:00  -ExclusionPath "$($env:USERPROFILE)\Documents\PowerShell" -verbose -ErrorAction stop}catch{exit 1}
 
 Exit 0
