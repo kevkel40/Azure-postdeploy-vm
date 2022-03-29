a7a5f5209ded50eeac2564347215435a99582f74
Leigh de Paor <leigh@spreachgeal.com>
2022-03-29 13:30:41 +0100

    Add SSMS

--- Bastion-Choco-Install-SW.ps1
+++ Bastion-Choco-Install-SW.ps1
@@ -43,6 +43,7 @@ switch( hostname ){
 			$packages += "stunnel"
 			$packages += "cosmosdbexplorer"
 			$packages += "jdk8"
+			$packages += "sql-server-management-studio"
 			#maven --version=3.6.3 ?
 		#redis cli
 		$url = "https://github.com/microsoftarchive/redis/releases/download/win-3.2.100/Redis-x64-3.2.100.msi"
